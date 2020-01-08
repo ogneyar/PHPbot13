d741c77f00ea08669ec3a9870cb3aa6a5b469fc2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-08 22:59:31 +0300

    Add files via upload

---
+++ web/a_conect.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$dbname = substr($url["path"], 1);
+	
+	$token = getenv("TOKEN_NEWTESTBOT");
+
+?>
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once 'Ne_wTest_Bot.php'; // Подключение бота
+
+?>
