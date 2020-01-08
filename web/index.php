fd91ff928db151df513a985bb9b0d737230febf0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-08 23:16:20 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 echo 'rrrtttyyyuuu';
-include_once 'Ne_wTest_Bot.php'; // Подключение бота
+
 echo "\n\n\n<h1>Вот те раз))</h1>";
+
+include_once 'Ne_wTest_Bot.php'; // Подключение бота
+
 ?>
