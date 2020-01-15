244fa26cca15cb01469d087bcdae302b6829aaf1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-15 20:38:23 +0300

    Add files via upload

--- web/InfoUsersBibla/Functions.php
+++ web/InfoUsersBibla/Functions.php
@@ -5,7 +5,7 @@ function _start_InfoUsers_bota() {
 
 	global $mess_from_fName, $bot, $mess_chat_id;
 	
-	$bot->sendMess($mess_chat_id, "Добро пожаловать, *".$mess_chat_id."*", markdown);	
+	$bot->sendMess($mess_chat_id, "Добро пожаловать, *".$mess_from_fName."*", markdown);	
 	
 }
 
