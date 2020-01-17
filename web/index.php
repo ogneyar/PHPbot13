2ec6932acba066486ce63feb7f139b64254ff6aa
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-17 23:47:23 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,52 @@
 <?php
 echo '111';
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+//use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 include_once '../vendor/autoload.php';
 include_once 'a_conect.php';
+
 echo '<br><br>222';
+
+
+
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+// Set PHPMailer to use the sendmail transport
+$mail->isSendmail();
+
+//Set who the message is to be sent from
+$mail->setFrom('ogneyar_ya@mail.ru', 'First Last');
+
+//Set an alternative reply-to address
+//$mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('ya13th@mail.ru', 'John Doe');
+
+//Set the subject line
+$mail->Subject = 'PHPMailer sendmail test';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+
+/*
+
+
+
+
+
 // `true` включает исключения (exceptions)
 $mail = new PHPMailer(true);
 
@@ -55,6 +95,10 @@ echo '777<br><br>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     
 echo '888<br><br>';
+
+
+*/
+
 
     $mail->send();
     echo 'Message has been sent';
