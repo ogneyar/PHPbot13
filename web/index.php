35237b4f68c9b50b7667f37e20dc023cf4d1a2da
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-01-17 23:36:56 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -37,7 +37,7 @@ echo '666<br><br>';
     //Recipients
     $mail->setFrom('ogneyar_ya@mail.ru', 'Mailer');
 //    $mail->addAddress('ya13th@mail.ru', 'Joe User');     // Add a recipient
-    $mail->addAddress('ogneyar-ne@yandex.ru');               // Name is optional
+//    $mail->addAddress('ogneyar-ne@yandex.ru');               // Name is optional
 //    $mail->addReplyTo('info@example.com', 'Information');
 //    $mail->addCC('cc@example.com');
 //    $mail->addBCC('ogneyar_ya@mail.ru');
