26da33b8f3e07383cff0bca8fa7d8ea27173b87a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-02-25 23:44:13 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -6,9 +6,6 @@ use PHPMailer\PHPMailer\Exception;
 include_once '../vendor/autoload.php';
 include_once 'a_conect.php';
 
-echo '222<br><br>';
-
-
 // Using Awesome https://github.com/PHPMailer/PHPMailer
 
 //require 'PHPMailerAutoload.php';
@@ -22,8 +19,8 @@ $mail->Username = $mail_smtp_login;   // SMTP username
 $mail->Password = $mail_smtp_pass;    // SMTP password
 $mail->SMTPSecure = 'tls';            // Enable encryption, only 'tls' is accepted
 
-$mail->From = 'ogneyar-ne@yandex.ru';
-$mail->FromName = 'Ogneyar';
+$mail->From = '@heroku.ru';
+$mail->FromName = 'PhpBot-NaHeroku';
 $mail->addAddress('ya13th@mail.ru');  // добавить получателя
 
 $mail->WordWrap = 50;                 // автоматический перенос символов
@@ -32,117 +29,12 @@ $mail->Subject = 'Hello';
 $mail->Body    = 'Testing some Mailgun awesomness';
 
 if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
+    echo 'Не смог отправить сообщение.';
+    echo 'Ошибка: ' . $mail->ErrorInfo;
 } else {
-    echo 'Message has been sent';
+    echo 'Сообщение отправлено!\n\n';
 }
 
-
-
-/*
-
-
-
-//Create a new PHPMailer instance
-$mail = new PHPMailer(true);
-
-try{
-
-// Set PHPMailer to use the sendmail transport
-$mail->isSendmail();
-
-//Set who the message is to be sent from
-$mail->setFrom('ogneyar_ya@mail.ru', 'First Last');
-
-//Set an alternative reply-to address
-//$mail->addReplyTo('replyto@example.com', 'First Last');
-
-//Set who the message is to be sent to
-$mail->addAddress('ya13th@mail.ru', 'John Doe');
-
-//Set the subject line
-$mail->Subject = 'PHPMailer sendmail test';
-
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-
- 
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';  
-  
-  
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-
-//Attach an image file
-//$mail->addAttachment('images/phpmailer_mini.png');
-
-
-/*
-
-
-// `true` включает исключения (exceptions)
-$mail = new PHPMailer(true);
-
-try {
-  
-   
-echo '<br><br>333';
-  
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    
-echo '<br><br>444<br><br>';
-    
-    $mail->Host       = 'mail.ru';                              // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'ogneyar_ya';                     // SMTP username
-    
-echo '555<br><br>';
-    
-    $mail->Password   = $passSMTP;                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
-
-    
-echo '666<br><br>';
-
-    
-    //Recipients
-    $mail->setFrom('ogneyar_ya@mail.ru', 'Mailer');
-//    $mail->addAddress('ya13th@mail.ru', 'Joe User');     // Add a recipient
-//    $mail->addAddress('ogneyar-ne@yandex.ru');               // Name is optional
-//    $mail->addReplyTo('info@example.com', 'Information');
-//    $mail->addCC('cc@example.com');
-//    $mail->addBCC('ogneyar_ya@mail.ru');
-
-    // Attachments
-//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    
-echo '777<br><br>';
-    
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    
-echo '888<br><br>';
-
-
-
-
-
-    $mail->send();
-    echo 'Message has been sent<br>';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}<br>";
-}
-
-*/
 // Подключаем файл бота
 include_once 'botInfoUsers.php';
 
